ca05d0e5e78f7ca129ba3abb4b91adf7accc2cf8
esbenscriver <esbenscriver@gmail.com>
2025-10-01 23:20:21 +0200

    use pandas for tabulating

--- run_simulations.py
+++ run_simulations.py
@@ -3,14 +3,12 @@ from jax import numpy as jnp
 
 import pandas as pd
 
-from tabulate import tabulate
-
 from matching import MatchingModel, Data, ModelParameters
 
 # Increase precision to 64 bit
 jax.config.update("jax_enable_x64", True)
 
-include_transfer_constant = False
+include_transfer_constant = True
 include_scale_parameters = True
 
 # Set dimensions
@@ -37,7 +35,7 @@ else:
 transfer_constant = jnp.asarray([1.0])
 
 # set mean and variance of measurement errors
-mu, sigma = transfer_constant, 0.001
+mu, sigma = transfer_constant, jnp.asarray([0.001])
 
 # Simulate data
 errors = mu + jnp.sqrt(sigma) * jax.random.normal(jax.random.PRNGKey(111), (X, Y))
@@ -59,7 +57,7 @@ parameter_names = parameter_names_X + parameter_names_Y
 if model.include_transfer_constant is True:
     parameter_names += ["salary constant"]
 if model.include_scale_parameters is True:
-    parameter_names += ["scale (X)", "scale (Y)"]
+    parameter_names += ["scale parameter (workers)", "scale parameter (firms)"]
 
 mp_true = ModelParameters(
     beta_X=beta_X,
@@ -87,49 +85,43 @@ estimates = model.fit(guess, data, maxiter=1, verbose=False)
 mp_estim = model.extract_model_parameters(estimates, transform=True)
 estimates_transformed = model.class2vec(mp_estim, transform=False)
 
-# Print estimated parameters
-print(
-    tabulate(
-        list(
-            zip(
-                parameter_names,
-                parameter_values,
-                guess,
-                estimates,
-                estimates_transformed,
-            )
-        ),
-        headers=[
-            "True parameter values",
-            "Parameter guess",
-            "Parameter estimates",
-            "Transformed estimates",
-        ],
-        tablefmt="grid",
+print("=" * 80)
+print("Parameter Estimates")
+print("=" * 80)
+df_estimates = (
+        pd.DataFrame(
+        {
+            "name": parameter_names,
+            "true parameters": parameter_values_transformed,
+            "estimated parameters": estimates_transformed,
+        }
     )
+    .round(3)
+    .set_index("name")
 )
+print(df_estimates)
+print("=" * 80)
+print(f"Number of estimated parameters: {len(df_estimates)}")
 print(f"number of observations: {observed_treansfer.size}\n")
 
 print(f"{model.neg_log_likelihood(parameter_values_transformed, data) = }")
 print(f"{model.neg_log_likelihood(estimates, data) = }\n")
 
-df_estimates = pd.DataFrame(
-    {
-        "name": parameter_names,
-        "estimates": estimates,
-        "transformed_estimates": estimates_transformed,
-    }
-).set_index("name")
-
 variance, mean = model.compute_moments(estimates, data)
-moment_estimates = jnp.asarray([mean, variance])
-moment_names = ["mean", "variance"]
 
-# Print estimated mean and variance of measurement errors
-print(
-    tabulate(
-        list(zip(moment_names, moment_estimates)),
-        headers=["Parameter estimates"],
-        tablefmt="grid",
+print("=" * 80)
+print("Estimated moments")
+print("=" * 80)
+df_moments = (
+    pd.DataFrame(
+        {
+            "name": ["mean", "variance"],
+            "true parameters": jnp.concatenate([mu, sigma], axis=0),
+            "estimated parameters": jnp.asarray([mean, variance]),
+        }
     )
+    .set_index("name")
+    .round(3)
 )
+print(df_moments)
+print("=" * 80)
